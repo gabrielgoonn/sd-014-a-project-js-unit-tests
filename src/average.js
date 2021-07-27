c251eee14a3b71c93dfcec5d278fa5d3fe5a8391
Vincenzo Fedzuirek Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2021-07-27 15:20:19 -0300

    1° Requisito

--- src/average.js
+++ src/average.js
@@ -14,8 +14,11 @@
 
 const average = (array) => {
   let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number' || array.length === 0) {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
     soma += array[index];
