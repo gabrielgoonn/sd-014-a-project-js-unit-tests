edc9d5838b719f94ab691097f90a2f2874ccbff7
BlyNTC <isaacase@hotmail.com>
2021-07-27 16:34:24 -0300

    requisito  9

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 
 /*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, 
+  Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, \
   cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
   - ler o menu cadastrado; 
   - fazer pedidos;
