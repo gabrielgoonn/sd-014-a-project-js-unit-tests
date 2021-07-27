4bc6d6e0440e78ca20b43e5adc94531a67d5159e
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-07-27 15:21:10 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  
+  let sum = 0;
+  
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / array.length); // Math.round é utilizado para arrendondar o número para o valor mais próximo. https://www.w3schools.com/js/js_math.asp
+};
 
 module.exports = average;
