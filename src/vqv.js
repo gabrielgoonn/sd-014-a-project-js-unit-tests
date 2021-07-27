1c49d77ed3d85f0418bc20f57a98b562b9406e60
danielknakano <81555352+danielknakano@users.noreply.github.com>
2021-07-27 15:49:56 -0300

    update vqv

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   } return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n
-    trabalho na Trybe e mando muito em programação!\n#VQV!`
+    trabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
