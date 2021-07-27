0550d24f72e59f2f0a9efd615ea96b7fd4ce6d15
Maurício Leite <mauricioleite1@gmail.com>
2021-07-27 19:21:10 -0300

    Requisito 01.2

--- src/average.js
+++ src/average.js
@@ -5,7 +5,7 @@ function check(item) {
 const average = (items) => {
   if (items.every(check) !== true || items.length === 0) {
     return undefined; 
-  };
+  }
   return Math.round(items.reduce((a, b) => a + b) / items.length); 
 };
 
