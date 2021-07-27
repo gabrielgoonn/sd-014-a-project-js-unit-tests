54e99a077232f5fa4da924680b69ec543143e9a1
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-27 17:28:23 -0300

    Adiciona o Requisito 2. Antes eu tinha adicionado o Requisito 4, confundindo com o 2.

--- src/numbers.js
+++ src/numbers.js
@@ -19,3 +19,10 @@ const numbers = (myArray) => {
 };
 
 module.exports = numbers;
+
+/*  const assert = require('assert');
+
+assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
+assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
+assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+assert.deepStrictEqual(numbers([' ']), false);  */
