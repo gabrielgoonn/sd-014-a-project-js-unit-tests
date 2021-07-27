bd4eb1346668e0d263901ef3c9ba4546b82ed8a2
Matheus Pozett <maa_souza11@hotmail.com>
2021-07-27 18:54:42 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const objetoName = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return objetoName;
+};
 
 module.exports = createStudent;
