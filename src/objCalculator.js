a026e4839109a23c3a7ee5d01a7a975db303d9c2
Danilo Menezes <danilomenezes66@gmail.com>
2021-07-27 19:02:10 -0300

    Desafio objCalculator + Lint

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,5 +20,17 @@
 */
 
 const calculator = {};
+const add = (n1, n2) => n1 + n2;
+const mult = (n1, n2) => n1 * n2;
+const div = (n1, n2) => Math.floor(n1 / n2);
+const sub = (n1, n2) => n1 - n2;
+
+calculator.add = add;
+calculator.mult = mult;
+calculator.div = div;
+calculator.sub = sub;
+
+// Função 'Math.floor() faz com que o valor decimal seja arredondado para o inteiro mais baixo'
+// Informação encontrada no site https://www.w3schools.com/jsref/jsref_floor.asp
 
 module.exports = calculator;
