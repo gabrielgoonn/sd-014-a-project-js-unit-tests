60aac1e35434dc107ec344d9405157c7b90fdef9
Bruno Feu <brunofeu89@gmail.com>
2021-07-27 18:26:37 -0300

    correcao no requisito 4, deixando o programa base original e alterando a conta da area do teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -40,6 +40,6 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // assert.strictEqual(Number(circle(3)['area'].toFixed(2)), 28.26);
     assert.strictEqual(parseFloat(circle(3)['area'].toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
+    assert.deepStrictEqual(circle(3), {radius: 3, area: (3.14 * 3 * 3), circumference: 18.84});
   });
 });
