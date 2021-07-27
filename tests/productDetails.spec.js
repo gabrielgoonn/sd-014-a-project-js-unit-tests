5adb1972c33c49c2d1806aa94041871bf742535f
Matheus Alves <malves224@gmail.com>
2021-07-27 19:55:06 -0300

    adiciona funçao de que percorre 3ultimos digitos do detalhe do produto de forma dinamica, cumprindo o requisito 6"

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,14 +28,32 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+const extraiTresUltimos = () => { // essa função percorre os 3 ultimos digitos do details dos produos de forma dinamica
+  let arrayComObjetos = Object.values(productDetails('Alcool gel', 'Máscara'));
+  let tresUltimosDigitos = '';
+  for (let index = 0; index < arrayComObjetos.length; index += 1) {
+    let arrayDetails = Object.values(arrayComObjetos[index].details)[0]; // extrai valor do objeto.details em um array com apenas uma posição, e acessa essa unica posição.
+    for (let indexReverse = arrayDetails.length - 3; indexReverse < arrayDetails.length; indexReverse += 1) {
+      tresUltimosDigitos = tresUltimosDigitos + arrayDetails[indexReverse];
+    }
+  }
+  return tresUltimosDigitos;
+}
+
+
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara')[0] !== productDetails('Alcool gel', 'Máscara')[1], true);
     // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(extraiTresUltimos(), '123123');
   });
 });
