508161db6a1c1dae1959f534061d586ede707049
Carla <carladmaximo@gmail.com>
2021-07-27 16:54:08 -0300

    Requisito 3

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+let soma = 0;
+let resultado = 0;
+const average = (array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number' || (array.length === 0)) {
+      return undefined;
+    } 
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    let numero = parseInt(array[index]);
+    soma += numero;
+    resultado = soma / array.length;
+  } return resultado;
 };
 
 module.exports = average;
