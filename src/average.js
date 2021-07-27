2993318fc00ebb0e299d373067f9552be139af1a
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-07-27 15:12:04 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let resultado = 0;
+  if (arr.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] === 'number') {
+      resultado += arr[index];
+    } else {
+      return undefined;
+    }
+  }
+  const media = Math.round(resultado / arr.length);
+  return media;
+};
 
 module.exports = average;
