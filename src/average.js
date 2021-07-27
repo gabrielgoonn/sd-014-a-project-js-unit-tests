508414f9bb85b298a228dda2c3ea7ad2ac41d156
João Victor Rodrigues Del Papa <joaodelpapa1@gmail.com>
2021-07-27 18:42:11 -0300

    Resquisito average.js

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let count = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      count += array[index];
+    } else {
+      return undefined;
+    }
+  }
+  return Math.round(count / (array.length));
+};
+  
 module.exports = average;
