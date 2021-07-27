aef1463db052cb66173563d444aa42d0c052c016
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-27 14:56:01 -0300

    Add requirement 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return { name: name, feedback: function () { return 'Eita pessoa boa!'} }
+};
 
 module.exports = createStudent;
