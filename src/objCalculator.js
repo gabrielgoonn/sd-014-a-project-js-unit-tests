8e6c6173887b46a5ec199f778537f4dd6765763b
Dogl4 <doougllas@hotmail.com.br>
2021-07-27 17:30:12 -0300

    c7 - Adicona funcao 'objCalculator', no arquivo 'ObjCalculator.js', usando arrow function.

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,13 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => a + b,
+  mult: (a, b) => a * b,
+  div: (a, b) => Math.floor(a / b), // Math.floor() Arredonda para baixo;
+  sub: (a, b) => a - b,
+};
+
+// Refencias <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor>
 
 module.exports = calculator;
