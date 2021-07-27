83c3f97fd570c0d6649f01ead63bee9a24e9f5e3
Gabriel Franklin <franklingabriel158@gmail.com>
2021-07-27 15:44:32 -0300

    Resolvido - Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,10 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (arr) => {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  
+  let media = 0;
+  let soma = 0;
 
-  // add your implemation here
-
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] !== 'number') {
+      return undefined;
+    }
+    soma += arr[index];
+    media = soma / arr.length;
+  }
+  return Math.round(media);
 };
 
 module.exports = average;
