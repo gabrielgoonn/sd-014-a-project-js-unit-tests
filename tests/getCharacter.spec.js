8a6862951b775347d87430f5845eb1307b22d5a7
Anna Hamann <annasoldih@gmail.com>
2021-07-27 18:28:18 -0300

    teste no req 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -51,15 +51,12 @@ describe('9 - Implemente os casos de teste para a função `getCharacter`', () =
     assert.strictEqual((getCharacter()), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
     assert.deepStrictEqual((getCharacter('Arya')), { name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'] });
-
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
     assert.deepStrictEqual((getCharacter('Brienne')), { name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     assert.deepStrictEqual((getCharacter('Melissandre')), { name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] });
     // Teste se a função se os parâmetros não são Case Sensitive.
     assert.deepStrictEqual((getCharacter('ArYa')), { name: 'Arya Stark', class: 'Rogue', phrases: ['Not today', 'A girl has no name.'] });
-    assert.deepStrictEqual((getCharacter('BrienNe')), { name: 'Brienne Tarth', class: 'Knight', phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'] });
-    assert.deepStrictEqual((getCharacter('MelisSandre')), { name: 'Melissandre', class: 'Necromancer', phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'] });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
     assert.deepStrictEqual(getCharacter('outro nome'), undefined);
   });
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -48,14 +48,18 @@ const createMenu = require('../src/restaurant');
 
 describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
     // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    assert.strictEqual(typeof (objetoRetornado), 'object');
+    assert.strictEqual(typeof (objetoRetornado.fetchMenu), 'function');
+
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
+    assert.strictEqual((Object.keys((createMenu({ food: {}, drink: {} })).fetchMenu)), ['food', 'drink']);
+ 
     // ```
     // const objetoRetornado = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
