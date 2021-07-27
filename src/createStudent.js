f4af5e65108985ffcb4c8c075344f18162c8d76d
Caio César Amorim <caiocavalcante063@gmail.com>
2021-07-27 17:49:00 -0300

    req. 05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let feedB = () => 'Eita pessoa boa!';
+  let obj = {
+    name,
+  };
+  obj.feedback = feedB;
+  return obj;
+};
+
+  console.log(createStudent('Caio'));
 
 module.exports = createStudent;
