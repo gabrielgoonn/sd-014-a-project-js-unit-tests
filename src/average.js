faf95ba7e0399f1145148063d3d93ec1a47de291
Filipe Brochier <filipe.brochier@gmail.com>
2021-07-27 14:33:26 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //Comentário feito para o commit inicial!
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+      soma += array[index];
+  } return Math.round(soma / array.length);
 };
 
 module.exports = average;
