7f823d188c79fdca0fe10af0d9193b72ccd5d795
AlineCarolina <eualine2017@gmail.com>
2021-07-27 17:43:19 -0300

    requisito 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,6 +1,9 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const {
+  strictEqual
+} = require('assert');
 const assert = require('assert');
 const numbers = require('../src/numbers');
 
@@ -19,9 +22,9 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     assert.fail();
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]));
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]));
+    assert.strictEqual(numbers([1, 'a', 3]));
+    assert.strictEqual(numbers([' ']));
   });
 });
