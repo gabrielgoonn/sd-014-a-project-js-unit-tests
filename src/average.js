9eb6f7ebc49513452d9eb00917e9025915019029
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-27 15:00:54 -0300

    Quase indo o requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if ((array.length > 0) && (array[i] !== (' ')) && (typeof array[i] === 'number')) {
+      soma += array[i];
+    } else {
+      return undefined;
+    }
+  }
+  if (soma !== 0) {
+    let media = soma / array.length;
+    return Math.round(media);
+  } else {
+    return 0;
+  }
+};
 
 module.exports = average;
