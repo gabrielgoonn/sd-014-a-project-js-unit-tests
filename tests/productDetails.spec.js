d7251af96e392b0285683360166c8c8d4302ab7d
Robert Nascimento <robert1a7x@gmail.com>
2021-07-27 17:06:38 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,27 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+
+    //Source: https://www.tutorialrepublic.com/faq/how-to-check-if-object-is-an-array-in-javascript.php#:~:text=Answer%3A%20Use%20the%20Array.,an%20array%3B%20otherwise%20returns%20false%20.
+    assert.strictEqual(Array.isArray(productDetails('Sabão', 'Miojo')), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Salgadinho', 'Ovo').length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('Cerveja', 'Vinho')[0]), 'object');
+    assert.strictEqual(typeof (productDetails('Cerveja', 'Vinho')[1]), 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Cerveja', 'Vinho')[0], productDetails('Cerveja', 'Vinho')[1]);
+
     // Teste que os dois productIds terminam com 123.
+    const firstProductId = productDetails('Bolacha', 'biscoito')[0].details.productId;
+    const secondProductId = productDetails('Bolacha', 'biscoito')[1].details.productId;
+
+    assert.strictEqual(firstProductId.slice(-3), '123');
+    assert.strictEqual(secondProductId.slice(-3), '123');
   });
 });
