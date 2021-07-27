e702cb4ce1bd856cadfb969b37b29e28b0877e58
JosueTavaress <josueferreira8.jf@gmail.com>
2021-07-27 18:34:53 -0300

    adiciona requisito 6 detalhes do objeto

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,61 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    // https://pt.stackoverflow.com/questions/77190/como-saber-se-um-objeto-%C3%A9-um-array-em-javascript-sem-jquery
+    assert.strictEqual(Array.isArray(productDetails()), true)
     // Teste que o array retornado pela função contém dois itens dentro.
+    const arr = productDetails();
+    assert.strictEqual(arr.length, 2)
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const tipos = () => {
+      if (typeof arr[0] === 'object' && typeof arr[1] === 'object'){
+        return true;
+      }else {
+        return false;
+      }
+    }
+    assert.strictEqual(tipos(), true)
     // Teste que os dois objetos são diferentes entre si.
+    const testeDoisObjetos = () => {
+      if (arr[0].name !== arr[1].name){
+        return false;
+      }else {
+        return true;
+      }
+    }
+    assert.strictEqual(testeDoisObjetos(), true)
     // Teste que os dois productIds terminam com 123.
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+    const numerosValidosU = 0;
+    const numerosValidosD = 0;
+    const testeTerminamComNumeros = () => {
+      if (arr[0].details.productId.length -1 === 3){
+        numerosValidosU += numerosValidosU;
+      }
+      if (arr[0].details.productId.length -2 === 2){
+        numerosValidosU += numerosValidosU;
+      }
+      if (arr[0].details.productId.length -3 === 1){
+        numerosValidosU += numerosValidosU;
+      }
+      if (arr[1].details.productId.length -1 === 3){
+        numerosValidosD += numerosValidosD;
+      }
+      if (arr[1].details.productId.length -2 === 2){
+        numerosValidosD += numerosValidosD;
+      }
+      if (arr[1].details.productId.length -3 === 1){
+        numerosValidosD += numerosValidosD;
+      }
+      if (numerosValidosD === numerosValidosU){
+        return true;
+      }else {
+        return false;
+      }
+    }
+    assert.strictEqual(testeTerminamComNumeros(), true)
   });
 });
