cfbeaba4eca7cddcfb80fb5355daa5516952cd4d
Lucas Duarte <lucs.rkt@gmail.com>
2021-07-27 16:44:53 -0300

    requisito 2 consertado

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -19,7 +19,7 @@ const numbers = require('../src/numbers');
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
-    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), false);  
+    assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);  
     assert.deepStrictEqual(numbers([1, 'a', 3]), false);
     assert.deepStrictEqual(numbers([' ']), false);
   });
