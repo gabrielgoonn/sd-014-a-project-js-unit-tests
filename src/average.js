70916f7ee487eeb2415ef0fddf2ee00a4f9fcab5
Andrea <andrealh08@gmail.com>
2021-07-27 18:42:56 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,35 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const verificaNaoNumero = (array) => {
+  let cont = 0;
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof (array[index]) !== 'number') {
+        cont += 1;
+      }
+    }
+    if (cont > 0) {
+      return true;
+    }
+    return false;
+};
 
-const average = () => {
-  
+const verificaSeVazio = (array) => {
+  if (array.length > 0) {
+    return false;
+  }
+  return true;
+};
+
+const average = (array) => {
+  if (verificaNaoNumero(array) === true || verificaSeVazio(array) === true) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let numero of array) {
+    sum += numero;
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
