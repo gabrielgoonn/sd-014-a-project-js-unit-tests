f5cd58029c8572a0bd338209e13e309be2e7dce3
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-27 14:54:07 -0300

    Implements numbers function test cases

--- src/average.js
+++ src/average.js
@@ -15,11 +15,11 @@
 const average = (arr) => {
   let result = 0;
   if (arr.length === 0) return;
-  for (let index = 0; index < arr.length; index += 1) {
-    if (typeof arr[index] !== 'number') return;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') return;
   }
-  for (let index = 0; index < arr.length; index += 1) {
-    result += arr[index];
+  for (let i = 0; i < arr.length; i += 1) {
+    result += arr[i];
   }
   return Math.round(result / arr.length);
 };
