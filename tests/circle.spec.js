ed923f2effde84c96c01a38ff176a22b51a31cb7
Augusto <augusto.raminelli>
2021-07-27 22:52:33 +0200

    requisito 4 test circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,11 +28,25 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(circle(), 'Object');
+    assert.strictEqual(typeof(circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    // estudei parseFloat no site https://www.youtube.com/watch?v=OXpTBQtL4nY
+    // estudei toPrecision no site https://www.youtube.com/watch?v=Yy3yIw1uhEA&list=RDCMUCc5FkTYiWH5L3Gk5IyW6Rmw&start_radio=1&rv=Yy3yIw1uhEA&t=40 
+    assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    });
   });
 });
+/* DICA: Números de ponto flutuante em JavaScript são imprecisos! Para testar, vá no seu navegador e faça `0.2 + 0.1`.
+Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
+Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!*/
