77fecf22cf6917302a98198e0dfd0d4f8f900538
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-27 20:08:41 -0300

    Adiciona o Requisito 6.

--- src/productDetails.js
+++ src/productDetails.js
@@ -40,3 +40,49 @@ const productDetails = (firstProduct, secondProduct) => [
 ];
 
 module.exports = productDetails;
+
+const assert = require('assert');
+
+/*  const ai = productDetails('Alcool gel', 'Máscara');
+console.log(ai);
+console.log(typeof ai);
+let isArray = Array.isArray(productDetails('Alcool gel', 'Máscara'));
+if (isArray) {
+  isArray = 'array';
+}
+console.log(isArray);
+console.log(typeof productDetails('Alcool gel', 'Máscara')[0]);
+console.log(typeof productDetails('Alcool gel', 'Máscara')[1]);
+let obj2 = [typeof productDetails('Alcool gel', 'Máscara')[0], typeof productDetails('Alcool gel', 'Máscara')[1]];
+if (obj2[0] === 'object' && obj2[1] === 'object') {
+  obj2 = 'object';
+}
+console.log(obj2);
+console.log(productDetails('Alcool gel', 'Máscara')[0]); 
+console.log(obj0);
+const obj0 = productDetails('Alcool gel', 'Máscara')[0];
+const obj1 = productDetails('Alcool gel', 'Máscara')[1];
+console.log(obj0.details.productId);
+const [obj0, obj1] = productDetails('Alcool gel', 'Máscara');
+const id0 = obj0.details.productId;
+let fim0 = [];
+const id1 = obj1.details.productId;
+let fim1 = [];
+console.log(id0);
+console.log(id1);
+let id123 = false;
+let c = 0;
+for (let i = id0.length - 1; i >= id0.length - 3; i -= 1) {
+  fim0.push(id0[i]);
+}
+for (let i = id1.length - 1; i >= id1.length - 3; i -= 1) {
+  fim1.push(id1[i]);
+}
+for (let i = 0; i < 3; i += 1) {
+  if (fim0[i] === fim1[i]) {
+    c += 1;
+  }
+}
+if (c === 3) {
+  id123 = true;
+} */
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,49 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+    let isArray = Array.isArray(productDetails('Alcool gel', 'Máscara'));
+    if (isArray) {
+      isArray = 'array';
+    }
+    assert.strictEqual(isArray, 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails('Alcool gel', 'Máscara')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    let obj2 = [typeof productDetails('Alcool gel', 'Máscara')[0], typeof productDetails('Alcool gel', 'Máscara')[1]];
+    if (obj2[0] === 'object' && obj2[1] === 'object') {
+      obj2 = 'object';
+    }
+    assert.strictEqual(obj2, 'object');
     // Teste que os dois objetos são diferentes entre si.
+    //  https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    const [obj0, obj1] = productDetails('Alcool gel', 'Máscara');
+    assert.notDeepStrictEqual(obj0, obj1);
     // Teste que os dois productIds terminam com 123.
+    const id0 = obj0.details.productId;
+    let fim0 = [];
+    const id1 = obj1.details.productId;
+    let fim1 = [];
+    console.log(id0);
+    console.log(id1);
+    let id123 = false;
+    let c = 0;
+    for (let i = id0.length - 1; i >= id0.length - 3; i -= 1) {
+      fim0.push(id0[i]);
+    }
+    for (let i = id1.length - 1; i >= id1.length - 3; i -= 1) {
+      fim1.push(id1[i]);
+    }
+    for (let i = 0; i < 3; i += 1) {
+      if (fim0[i] === fim1[i]) {
+        c += 1;
+      }
+    }
+    if (c === 3) {
+      id123 = true;
+    }
+    assert.strictEqual(id123, true);
   });
 });
