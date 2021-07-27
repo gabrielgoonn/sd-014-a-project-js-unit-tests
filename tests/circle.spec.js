62045dedbb1cac135d40ee391485963494ba50f3
Dogl4 <doougllas@hotmail.com.br>
2021-07-27 16:02:24 -0300

    c4 - Adiciona os teste para a funcao 'circle', no arquivo 'circle.spec.js'.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,25 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.deepStrictEqual(typeof circle(180), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(Object.keys(circle(180)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepStrictEqual(Object.values(circle(2))[0], 2); // usei o console do navegador para achar o resultado esperado.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(Math.round(Object.values(circle(3))[1]), 28); // usei o console do navegador para achar o resultado esperado.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const arredondaArry = (arr) => {
+      let arr2 = [];
+      for (let i of arr) {
+        arr2.push(Math.round(i))
+      }
+      return arr2;
+    }; // arredonda para verificar, os valores retornados.
+    assert.deepStrictEqual(arredondaArry(Object.values(circle(3))), [3, 28, 19]);
   });
 });
