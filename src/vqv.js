ab6a7148e0e818b80f6d6460cd1d91c515034d68
tiagosathler <sathler@gmail.com>
2021-07-27 16:26:01 -0300

    Requisito 3: concluído

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  let result = `Oi, meu nome é ${nome}!\n`;
+  result += `Tenho ${idade} anos,\n`;
+  result += 'trabalho na Trybe e mando muito em programação!\n';
+  result += '#VQV!';
+  return result;
+};
 
 module.exports = vqv;
