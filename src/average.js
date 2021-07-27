99b2481c2f609b8424069092edbe3781d3b77665
BrianCaetano <brian-caetano@hotmail.com>
2021-07-27 15:27:08 -0300

    requisito 4 ok

--- src/average.js
+++ src/average.js
@@ -14,13 +14,13 @@
 
 const average = (arr) => {
   if (arr.length === 0) {
-    return undefined
+    return undefined;
   }
 
   let soma = 0;
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof arr[i] !== 'number'){
-      return undefined
+    if (typeof arr[i] !== 'number') {
+      return undefined;
     }
     soma += arr[i];
   }
