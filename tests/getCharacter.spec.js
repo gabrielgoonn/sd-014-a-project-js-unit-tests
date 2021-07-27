b57ab33fdb2995ad7d2bbc291426ed7e6fd391ba
MuriloM <muriloommaia@gmail.com>
2021-07-27 16:44:27 -0300

    requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -46,13 +46,35 @@ OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enq
 
 describe('9 - Implemente os casos de teste para a função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(getCharacter(), undefined);
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    const correctArya = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.'],
+    };
+    assert.deepStrictEqual(getCharacter('arya'), correctArya);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    const correctBrienne = {
+        name: 'Brienne Tarth',
+        class: 'Knight',
+        phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+      };
+      assert.deepStrictEqual(getCharacter('brienne'), correctBrienne);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    const correctMelissandre = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    };
+    assert.deepStrictEqual(getCharacter('melissandre'), correctMelissandre);
     // Teste se a função se os parâmetros não são Case Sensitive.
+    const testeFull = [getCharacter('ArYa'), getCharacter('brIeNNe'), getCharacter('meLisSaNdre')];
+    assert.deepStrictEqual(testeFull, [correctArya, correctBrienne, correctMelissandre]);
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.strictEqual(getCharacter('murilo'), undefined);
   });
 });
