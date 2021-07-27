eaa8deacd9d1886362c61f36c82f6795a712cd95
Anna Hamann <annasoldih@gmail.com>
2021-07-27 19:22:05 -0300

    conserta product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,21 +28,21 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de ok para a função `productDetails`', () => {
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // ESCREVA SEUS okS ABAIXO:
-    // let ok = productDetails('Alcool gel', 'Máscara');
-    // // ok que o retorno da função é um array.
-    // assert.strictEqual(typeof productDetails(), 'object');
-    // // ok que o array retornado pela função contém dois itens dentro.
-    // assert.strictEqual(productDetails().length, 2);
-    // // ok que os dois itens dentro do array retornado pela função são objetos.
-    // assert.strictEqual(typeof ok[0], 'object');
-    // assert.strictEqual(typeof ok[1], 'object');
-    // // ok que os dois objetos são diferentes entre si.
-    // assert.notStrictEqual(ok[0], ok[1]);
-    // // ok que os dois productIds terminam com 123.
-    // assert.strictEqual(ok[0].details.productId.substr(ok[0].details.productId.length - 3), '123');
-    // assert.strictEqual(ok[1].details.productId.substr(ok[1].details.productId.length - 3), '123');
+    let ok = productDetails('Alcool gel', 'Máscara');
+    // ok que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(), 'object');
+    // ok que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
+    // ok que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof ok[0], 'object');
+    assert.strictEqual(typeof ok[1], 'object');
+    // ok que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(ok[0], ok[1]);
+    // ok que os dois productIds terminam com 123.
+    assert.strictEqual(ok[0].details.productId.substr(ok[0].details.productId.length - 3), '123');
+    assert.strictEqual(ok[1].details.productId.substr(ok[1].details.productId.length - 3), '123');
   });
 });
