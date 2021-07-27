a11f40bbd2f5ea9bf60c56fda329ba35368bed36
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-07-27 18:32:56 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,13 +14,24 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
-
+/* 
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
+    }
+  }
+  return myArray;
+};
+*/
+const myCounter = () => {
+  let myArray = [];
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
