fa626248875e3058cf9ea5cb553b7157a8ee99c8
João Ferreira <joaovtf39@gmail.com>
2021-07-27 16:46:29 -0300

    add requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,12 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => 
- if (nome === ''
-
-`Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  if (typeof nome !== 'string' & typeof idade !== 'number') {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
+}
+
 
 module.exports = vqv;
+
