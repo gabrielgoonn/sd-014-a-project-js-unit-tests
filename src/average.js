abc7608ccadbcdd4716850218b445948e03207a0
Pally <francisca.bezerr@gmail.com>
2021-07-27 17:49:18 -0300

    requisito 1 ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  if (array === undefined || array.length === 0) { // undefined representa um valor indefinido(caso sem parâmetro) ou array vazio [0 elementos]
+    return undefined;
+  }
+  let total = 0;
+  for (let index of array) {
+    if (typeof index !== 'number') { // preciso verificar cada index pra vê se é number. Poderia ser usado (!Number.isFinite(index)) ao invés de typeof. O método Number.isFinite() determina se o valor passado é um número finito. 
+      return undefined; 
+    }
+    total += index;
+  }
+  return Math.round(total / array.length); // A função Math.round() retorna o valor de um número arredondado para o inteiro mais proximo (cumprindo ao requisito de que todo os resultados devem ser arredondados para valores inteiros)
+};
+// A Média Aritmética de um conjunto de dados é obtida somando todos os valores e dividindo o valor encontrado pelo número de dados desse conjunto.
 module.exports = average;
