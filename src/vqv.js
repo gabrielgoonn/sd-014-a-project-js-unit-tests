df4675a8cd75132b60441fa9074210a74a62d4aa
Michel <ferrazmichel@gmail.com>
2021-07-27 16:42:37 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof nome === 'undefined') {
+    let text;
+    return text;
+  }
+  const texto = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return texto;
+};
 
 module.exports = vqv;
