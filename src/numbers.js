cfd2bd6e45ea25d3a95999d22b3e9fdc24e15dc3
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-27 17:52:38 -0300

    requisito numbers 100% ok

--- src/numbers.js
+++ src/numbers.js
@@ -8,6 +8,7 @@
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
 */
+const assert = require('assert');
 
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
@@ -18,4 +19,8 @@ const numbers = (myArray) => {
   return true;
 };
 
+assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
+assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
+assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+assert.deepStrictEqual(numbers([' ']), false);
 module.exports = numbers;
