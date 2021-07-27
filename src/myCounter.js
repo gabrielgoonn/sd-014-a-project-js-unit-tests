27f502ba5d610ff60608cb31745e1180eae2db37
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-27 17:12:50 -0300

    myCounter 100% ok lint corrigido

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counters = 2; counters <= 3; counters += 1) {
+      myArray.push(counters);
     }
   }
   return myArray;
