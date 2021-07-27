fff40ca43f9beb9a9462ce3343aae6d283795479
Semnome1505 <Gabriel_1505@outlook.com>
2021-07-27 16:58:43 -0300

    Adiciona o Requsito 2 novamente, mudando somente o arquivo circle.spec.js.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,8 +35,12 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area, 28.26);
+    let areaCirculo = circle(3).area;
+    areaCirculo = parseFloat(areaCirculo.toPrecision(4));
+    assert.strictEqual(areaCirculo, 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
+    let circulo = circle(3);
+    circulo.area = areaCirculo;
+    assert.deepStrictEqual(circulo, { radius: 3, area: 28.26, circumference: 18.84 });
   });
 });
