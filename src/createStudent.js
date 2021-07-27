bdfaaf2486b075290947a307b0bc5480d33df7bb
Welton Lopes <weltonlopesmetal@gmail.com>
2021-07-27 17:20:50 -0300

    Eslint corrigindo

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,8 +17,6 @@
 
 const mens = () => 'Eita pessoa boa!';
 
-const createStudent = (name) => {
-  return estudante = { name, feedback: mens };
-};
+const createStudent = (name) =>estudante = { name, feedback: mens };
 
 module.exports = createStudent;
