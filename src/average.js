8114bb2cc49914a33b05b64899a3299bff020d2b
WalmirLucena <walmirlucena3@gmail.com>
2021-07-27 14:04:53 -0400

    Adiciona a funçao average

--- src/average.js
+++ src/average.js
@@ -11,8 +11,19 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    sum += array[index];  
+  }
+  return Math.round(sum / array.length);
+};
 // starting project
 
 module.exports = average;
