c5e994e405486bb0df06e04144c42ea5c9c066f0
Lys <lysprestes@gmail.com>
2021-07-27 15:35:16 -0300

    3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,11 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) { // Caso o nome ou a idade sejam undefined, retorna undefined
+    return undefined;
+  } // Caso contrário, retorna o parágrafo descrito abaixo
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+};
 
 module.exports = vqv;
