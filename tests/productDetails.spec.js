37af7bfafc9e53254f0e8760b98bfe12a5eb3bfb
matheusmattos7 <matheusm.mattos7@gmail.com>
2021-07-27 19:03:59 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails().length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails(), 'object');
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1], true);
+
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.substr(productDetails()[0].details.productId.length - 3), '123');
+    assert.strictEqual(productDetails()[1].details.productId.substr(productDetails()[1].details.productId.length - 3), '123');
+    // Usamos o substr para pegar os três ultimos caracteres da string e confirmamos que eles são '123' como segundo parametro do substr!
   });
 });
