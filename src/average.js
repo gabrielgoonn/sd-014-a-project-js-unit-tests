d6061d4c903b53577ad74f8f26c4df6caa8d1040
Amanda Zotelli <amzotelli@gmail.com>
2021-07-27 20:56:50 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (param) => {
+  if (param.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < param.length; i += 1) {
+    if (typeof param[i] !== 'number') {
+      return undefined;
+    }
+  }
+  return Math.round(param.reduce((somaValores, valorAtual) =>
+    somaValores + valorAtual, 0) / param.length);
+};
 
 module.exports = average;
