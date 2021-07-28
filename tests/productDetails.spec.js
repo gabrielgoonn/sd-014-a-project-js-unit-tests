b9b837f8f229c4001b809a4a70bd56c91a7187b3
Leonardo <leonardodiber@gmail.com>
2021-07-28 10:41:59 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { type } = require('os');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -29,13 +30,23 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {    
     // ESCREVA SEUS TESTES ABAIXO:
+    const array = productDetails('alface', 'bola');
     // Teste que o retorno da função é um array.
+    // Verificar typeof array no link a seguir
+    // https://stackoverflow.com/questions/12996871/why-does-typeof-array-with-objects-return-object-and-not-array
+    assert.strictEqual(array instanceof Array, true, 'Retorno deve ser um array')
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(array.length, 2, 'Array deve conter 2 elementos')
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(array[0]), 'object', 'Elemento deve ser um objeto')
+    assert.strictEqual(typeof(array[1]), 'object', 'Elemento deve ser um objeto')
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(array[0] !== array[1], true, 'Os objetos não podem ser iguais')
     // Teste que os dois productIds terminam com 123.
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+    const idCheck = array.every((product) => product.details.productId.slice(-3) === '123');
+    assert.strictEqual(idCheck, true)    
   });
 });
