21752b4d6dbe63bd4b763a84bb808b1b8c8b268c
jagrunewald <je_grunewald@hotmail.com>
2021-07-27 23:22:09 -0300

    Requisito 8 - funçao MyCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,14 +15,15 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
+// Requisito 8
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
-  }
+  var myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  // for (let counter = 0; counter <= 3; counter += 1) {
+  //   myArray.push(counter);
+  //   for (let counter = 2; counter <= 3; counter += 1) {
+  //     myArray.push(counter);
+  //   }
+  // }
   return myArray;
 };
 
