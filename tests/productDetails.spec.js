228a1733ec66b3c399b4249389593bc3aae8e2b1
Victor hugo Souza <victoraster2014@gmail.com>
2021-07-27 22:01:04 -0300

    adicionando correçao do req. 9

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,7 +37,10 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(produtosTeste.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof produtosTeste, 'object'); 
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(produtosTeste[0],produtosTeste[1]); 
     // Teste que os dois productIds terminam com 123.
+    // assert.strictEqual(produtosTeste.details.productId , 
   });
 });
