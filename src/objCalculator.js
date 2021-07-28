4b697e8be0cdafb69cdc9b5556e63282219d292d
Eduardo Ortulan Roberto <eduortulan@gmail.com>
2021-07-27 21:59:10 -0300

    Funçao calculator - completa

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => Math.floor(a + b),
+  mult: (c, d) => Math.floor(c * d),
+  div: (e, f) => Math.floor(e / f),
+  sub: (g, h) => Math.floor(g - h),
+};
 
 module.exports = calculator;
