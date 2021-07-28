184dd7ea5344736d3baa25b9c4fe4a2d0c9df3ae
Elielson Nascimento <nascimento.elielson@gmail.com>
2021-07-28 09:25:07 -0300

    Requisito create student

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = {
+    name,
+    feedback() {
+      const goodPerson = 'Eita pessoa boa!';
+      return goodPerson;
+    },
+  };
+  return estudante;
+};
 
 module.exports = createStudent;
