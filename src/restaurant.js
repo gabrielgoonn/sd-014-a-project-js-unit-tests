a49546933dc2396e7feeba2d1464b26897578c86
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-27 21:56:54 -0300

    lint

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,17 +79,17 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, 
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 const restaurant = {};
+const orderFromMenu = (request) => restaurant.consumption.push(request);
 
 const createMenu = (myMenu) => {
   Object.assign(restaurant, {
     fetchMenu: () => myMenu,
     consumption: [],
     order: (request) => orderFromMenu(request),
-    pay: () => {}
-  })
+    pay: () => {},
+  });
   return restaurant;
 };
 
-const orderFromMenu = (request) => restaurant.consumption.push(request);
 
 module.exports = createMenu;
