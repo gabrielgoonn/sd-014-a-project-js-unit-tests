746c5dd9441b954b9af4821c046b14a6da3f03ec
daniel <danielknakano@gmail.com>
2021-07-28 16:20:59 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  let myArray = [];
+  for (let x = 0; x <= 3; x++) {
+    myArray.push(x);
+    for (y = 2; y <= 3; y++) {
+      myArray.push(y);
     }
   }
   return myArray;
