f4e71b09e2f959817a39a5925477d076360bf9fa
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 12:45:39 -0300

    Add test to test 6 deepstrictequal

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { checkPrimeSync } = require('crypto');
 const createMenu = require('../src/restaurant');
  
 /*
@@ -86,11 +87,12 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
     // ```
     // objetoRetornado.order("coxinha");
-    objetoRetornado.order("agua");
-    objetoRetornado.order("sopa");
-    objetoRetornado.order("sashimi");
+    objetoRetornado.order('agua');
+    objetoRetornado.order('sopa');
+    objetoRetornado.order('sashimi');
     // objetoRetornado.consumption // Retorno: ["coxinha", "agua", "sopa", "sashimi"]
     assert.strictEqual(objetoRetornado.consumption.length, 4);
+    assert.deepStrictEqual(objetoRetornado.consumption, ['coxinha', 'agua', 'sopa', 'sashimi'])
     // Agora faça o TESTE 7 deste arquivo.
     // --------------------------------------------------------------------------------------
     // TESTE 7: Verifique que a função `order` aceita que pedidos repetidos sejam acrescidos a consumption.
