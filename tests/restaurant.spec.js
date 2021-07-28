fe4c39941c36cf9affab30a860242a7168b23582
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 11:42:57 -0300

    Create first test restaurants

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,8 +51,9 @@ describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
     // assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
     // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
-    // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    assert.deepStrictEqual(Object.keys(objetoRetornado).filter((key) => key === 'fetchMenu'), ['fetchMenu']);
+
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
