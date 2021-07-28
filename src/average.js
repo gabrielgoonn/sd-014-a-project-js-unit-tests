5d7bef4734f465a91fa9f755a100f8cd03b5483e
jagrunewald <je_grunewald@hotmail.com>
2021-07-27 21:14:36 -0300

    Requisito 1 - funçao average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  const sizeArray = array.length;  
+  
+  if (array.length === 0) {
+    return undefined;
+  } 
+  
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  
+  let media = sum / sizeArray;
+  return Math.round(media);
+  };
 
 module.exports = average;
