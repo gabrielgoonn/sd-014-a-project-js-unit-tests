ac914ded5375b2d6f59b0b70fdc6d5239b9b8af2
juli0c3sar <jcmuchiutti@gmail.com>
2021-07-28 20:18:45 -0300

    exercicio createstudent.js - finalizado

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameref) => ({
+    name: nameref,
+    feedback: () => 'Eita pessoa boa!',
+  });
 
 module.exports = createStudent;
