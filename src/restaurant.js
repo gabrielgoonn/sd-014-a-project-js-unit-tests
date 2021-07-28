79dda8c665440ce7ee7c5c04ea41d6131297beca
unamednada <dias.gustavo.dias@gmail.com>
2021-07-28 12:03:44 -0300

    Add key consumption to createMenu

--- src/restaurant.js
+++ src/restaurant.js
@@ -46,7 +46,7 @@
 */
 
 // PASSO 1: Crie uma função `createMenu()` que, dado um objeto passado por parâmetro, retorna um objeto com o seguinte formato: { fetchMenu: () => objetoPassadoPorParametro }.
-const createMenu = (menu) => ({ fetchMenu: () => menu });
+const createMenu = (menu) => ({ fetchMenu: () => menu, consumption: [] });
 // Agora faça o TESTE 4 no arquivo `tests/restaurant.spec.js`.
 
 //------------------------------------------------------------------------------------------
