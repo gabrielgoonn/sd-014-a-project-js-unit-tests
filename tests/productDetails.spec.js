19afc8cb468559fabdd67154734f8c92df53d338
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-07-28 20:53:03 -0300

    Requisito 6 corrigido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -38,7 +38,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara'));
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
     // Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0].details.productId, "Alcool gel123");
     assert.strictEqual(productDetails('Alcool gel', 'Máscara')[1].details.productId, "Máscara123");
