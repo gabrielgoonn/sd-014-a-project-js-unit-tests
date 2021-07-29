201f801549fec4cfe2a39cb2317e3228710c1651
p-lacerda <xcloudgameemail@gmail.com>
2021-07-29 20:45:11 -0300

    Resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const newObject = { name: nome, feedback: () => 'Eita pessoa boa!' };
+  return newObject;
+};
 
 module.exports = createStudent;
