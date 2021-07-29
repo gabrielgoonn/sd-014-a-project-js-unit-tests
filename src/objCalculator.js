016be5e423246544f8e53f092f73f857d1320f9a
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-07-29 12:44:36 -0300

    resolvido o requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,12 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (number1, number2) => (number1 + number2),
+  mult: (number1, number2) => (number1 * number2),
+  // Referência: w3schools https://www.w3schools.com/jsref/jsref_parseint.asp
+  div: (number1, number2) => parseInt(number1 / number2, 10),
+  sub: (number1, number2) => (number1 - number2),
+};
 
 module.exports = calculator;
