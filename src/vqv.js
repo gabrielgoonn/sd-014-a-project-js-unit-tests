2b6ee41b3fadd0bee84c762973bfe70c25a57e7a
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2021-07-29 12:23:08 -0300

    Correction lint

--- src/vqv.js
+++ src/vqv.js
@@ -17,9 +17,9 @@
 */
 
 const vqv = (nome, idade) => {
-  if(!nome && !idade) {
+  if (!nome && !idade) {
     return
-  } 
+  }
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
