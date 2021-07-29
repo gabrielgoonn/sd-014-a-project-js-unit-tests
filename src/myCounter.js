5d308343844eeee0b0d23d5d325652ddd8bd9351
Amanda Zotelli <amzotelli@gmail.com>
2021-07-29 20:12:50 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,15 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-// const myCounter = () => {
-//   let myArray = [];
-//   for (var counter = 0; counter <= 3; counter += 1) {
-//     myArray.push(counter);
-//     for (var counter = 2; counter <= 3; counter += 1) {
-//       myArray.push(counter);
-//     }
-//     return myArray;
-//   }
-// };
+const myCounter = () => {
+  let myArray = [];
+  for (let i = 0; i <= 3; i += 1) {
+    myArray.push(i);
+    for (let b = 2; b <= 3; b += 1) {
+      myArray.push(b);
+    }
+  }
+  return myArray;
+};
 
-// module.exports = myCounter;
+module.exports = myCounter;
