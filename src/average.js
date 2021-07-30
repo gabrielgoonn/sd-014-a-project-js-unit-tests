f4eda2cd38e4b2b71d6832fea2c016efa15d792c
Rogério Maia Jr. [Roger] <rogeriomaiajr13@gmail.com>
2021-07-30 15:34:05 -0300

    RQ 01 ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') return undefined;
+    sum += array[index];
+  }
+  const result = Math.round(sum / array.length);
+  return result;
+};
 
 module.exports = average;
