78b1b32ba05e29528c18a68fa2168824a368f3ea
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2021-07-30 19:58:12 -0300

    Resolucao quesito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -28,14 +28,30 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+describe("6 - Implemente os casos de teste para a função `productDetails`", () => {
+  it("Verifica se a função `productDetails` tem o comportamento esperado", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('a', 'b')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('a', 'b').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    productDetails('a', 'b').forEach((parametro) => {
+      assert.strictEqual(typeof parametro, 'object');
+    });
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(
+      productDetails('a', 'b')[0] !== productDetails('a', 'b')[1],
+      true
+    );
     // Teste que os dois productIds terminam com 123.
+    assert.strictEqual(
+      productDetails('a', 'b')[0].details.productId.slice(-3),
+      '123'
+    );
+    assert.strictEqual(
+      productDetails('a', 'b')[1].details.productId.slice(-3),
+      '123'
+    );
   });
 });
