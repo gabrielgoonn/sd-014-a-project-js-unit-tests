ff4ea54dc16b686f35994d52ba9dd2b3407535bf
Riquelme <riquelmebandeira1@outlook.com>
2021-07-31 09:19:55 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,35 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const typeCheck = (productList) => {
+      return productList.every((product) => 
+        typeof product === 'object'
+      )
+    };
+    assert.strictEqual(typeCheck(productDetails()), true);
+
     // Teste que os dois objetos são diferentes entre si.
+    const differentElement = (productList) => {
+      if (productList[0].name === productList[1].name) {
+        return false;
+      } 
+      return true;
+    }
+    
+    const newProductList = productDetails('Máscara', 'Álcool Gel');
+    assert.strictEqual(differentElement(newProductList), true);
+
     // Teste que os dois productIds terminam com 123.
+    const endCheck = (productList) => productList.every((product) => product.details.productId.endsWith('123'));
+    assert.strictEqual(endCheck(newProductList), true);
   });
 });
