40ec4c31d4c9d81c10dd4e6aa70974d2a4cca64d
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-07-31 01:07:41 -0300

    requisito 01 terminado

--- src/average.js
+++ src/average.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 /*
 starting....
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
@@ -12,7 +13,29 @@ starting....
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+  // vamos declarar uma função com um array como parâmetro.
+const average = (array) => {
+  if (array.length === 0) {
+     // se o array for vazio retorna undefined
+    return undefined;
+  }
+  let sum = 0;
+  for (const number of array) {
+  // percorre array para testarmos se os elementos são de fato números.
+    if (typeof number === 'number') {
+      sum += number;
+    } else {
+      return undefined;
+    }
+  }
+  return Math.round(sum / array.length);
+};
 
-const average = () => {};
+// assert.strictEqual(expected, 2);
+// assert.strictEqual(expected, 1);
+// fontes:
+// https://onecompiler.com/posts/3vyd4sbdq/finding-the-average-value-of-an-array-in-javascript-using-arrow-functions
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// idéia dos colegas da trybe
 
 module.exports = average;
