63cebcd36b4b12a9cb478c97fd6b8b3d2640e58b
Leonidas Junior <leonautcco@gmail.com>
2021-08-01 01:51:08 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterInArray = 2; counterInArray <= 3; counterInArray += 1) {
+      myArray.push(counterInArray);
     }
   }
   return myArray;
