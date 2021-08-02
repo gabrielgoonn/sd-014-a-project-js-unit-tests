a4311e63df56bb79c71c625b216ce9ec551a6b8b
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-08-02 17:09:29 +0100

    ajustando linter

--- src/average.js
+++ src/average.js
@@ -15,14 +15,14 @@
 const average = (array) => {
   let soma = 0;
 
-if (array.length ===0) return undefined;
+if (array.length === 0) return undefined;
 
   for (let index = 0; index < array.length; index += 1) {
-    if  (typeof array[index] !== 'number') return undefined;
+    if (typeof array[index] !== 'number') return undefined;
     soma += array[index];
   }
 
-  return Math.round(soma/array.length);
+  return Math.round(soma / array.length);
 };
 
 module.exports = average;
