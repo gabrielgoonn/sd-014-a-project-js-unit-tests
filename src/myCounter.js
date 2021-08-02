84b3415e148631175abb940459ca45b1610f3a81
Veruska de Oliveira <veruska.ideasdigital@gmail.com>
2021-08-03 00:27:16 +0100

    Correçao #VQV

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,12 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let SecondCounter = 2; SecondCounter <= 3; SecondCounter += 1) {
+      myArray.push(SecondCounter);
     }
   }
   return myArray;
 };
-
 module.exports = myCounter;
--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome !== 'string' || typeof idade !== 'number') {
-    return undefined;
-  }
- 
-   return `Oi, meu nome é ${nome}!
- Tenho ${idade} anos,
- trabalho na Trybe e mando muito em programação!
- #VQV!`;
- };
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    let result = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+    return result;
+  } return undefined;
+};
+
+module.exports = vqv;
