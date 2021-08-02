8f03273972bc852db5ccc2432f1a728e6462638d
DanFilipe2000 <anielilipe90@gmail.com>
2021-08-02 15:45:06 -0300

    Requisito 2

--- src/numbers.js
+++ src/numbers.js
@@ -9,6 +9,8 @@
     - numbers([2, 'errado', 5]); // Retorna: false
 */
 
+const assert = require('assert');
+
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
@@ -19,3 +21,8 @@ const numbers = (myArray) => {
 };
 
 module.exports = numbers;
+
+assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
+assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
+assert.strictEqual(numbers([1, 'a', 3]), false);
+assert.strictEqual(numbers([" "]), false);
