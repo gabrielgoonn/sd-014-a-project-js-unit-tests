d3152c6b6a96eb43d7859fe6a090a319502a24f1
Carla <carladmaximo@gmail.com>
2021-08-02 12:39:46 -0300

    Requisito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,9 +17,9 @@
 
 const myCounter = () => {
   let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
