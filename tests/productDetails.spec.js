d1ce9fd5d085f1a4fa14f399019926a6ee27b4df
jmporto <jmporto@outlook.com>
2021-08-02 13:12:28 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,20 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+    assert.strictEqual(Array.isArray(productDetails('a', 'b')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length,2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails(),'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(Object.entries(productDetails()[0]),Object.entries(productDetails()[1])); 
     // Teste que os dois productIds terminam com 123.
+    const Entry1 = productDetails()[0].details.productId.slice(-3);
+    const Entry2 = productDetails()[1].details.productId.slice(-3);
+    assert.strictEqual(Entry1.endsWith('123'), true);
+    assert.strictEqual(Entry2.endsWith('123'), true);
   });
 });
