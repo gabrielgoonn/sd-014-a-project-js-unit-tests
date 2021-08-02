f6e464fac9166fdad8e87d5c11935b7829469529
jmporto <jmporto@outlook.com>
2021-08-02 12:57:17 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,10 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
+const fraseBoa = () => 'Eita pessoa boa!';
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: fraseBoa,
+});
 
 module.exports = createStudent;
