fc42378ec430d3af8a8e165d711dce9a5ae420b8
jhonathancs <jhonathan_jcs@hotmail.com>
2021-08-02 22:10:26 -0500

    item eslint
    corrigido counter

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,15 +16,15 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
+  let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (let Counter = 2; Counter <= 3; Counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
-// console.log(myCounter);
+console.log(myCounter);
 
 module.exports = myCounter;
