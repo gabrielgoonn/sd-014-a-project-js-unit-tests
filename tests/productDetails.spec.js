37619f0d01acd6694d28689aff4777c587f88a1e
Fernando Monteiro <fernandoflmbatera@gmail.com>
2021-08-03 10:42:47 -0300

    Correção na função productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,17 +33,17 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails), true);
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails[0], 'object');
-    assert.strictEqual(typeof productDetails[1], 'object');
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails[0], productDetails[1]);
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
     // Teste que os dois productIds terminam com 123.
-    assert.ok(productDetails[0].endsWith('123'));
-    assert.ok(productDetails[1].endsWith('123'));
+    assert.ok(productDetails('Alcool gel', 'Máscara')[0].details.productId.endsWith('123'));
+    assert.ok(productDetails('Alcool gel', 'Máscara')[1].details.productId.endsWith('123'));
   });
 });
 
