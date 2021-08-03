2e4500e7ef29a4d2319601c901e971cf6c82af59
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-08-03 13:40:59 -0300

    vqv  requirement fixed code  and  completed

--- src/vqv.js
+++ src/vqv.js
@@ -16,14 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {  
-  if (nome || idade === undefined) {
-    return undefined;
-  }
-  return `Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
+  }
+  return undefined;
 };
 
 // fonte : ajuda dos colegas
