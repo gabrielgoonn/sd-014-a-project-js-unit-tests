dc86e998801059df81bb2f2f298a0506e1a62bbf
julianoprogrammierer <julianoprogrammierer@gmail.com>
2021-08-03 09:03:11 -0300

    getCharacter  requirement completed

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -6,53 +6,74 @@ const getCharacter = require('../src/getCharacter');
 
 /*
 Essa função recebe como parâmetro o nome de um personagem e retorna um objeto com seu nome, classe e frases.
-
 O retorno será de acordo com a seguinte relação:
-
  Parâmetro  |      Nome       |    Classe   |              Frases
 ----------------------------------------------------------------------------------
    Arya     |   Arya Stark    |    Rogue    | 'Not today', 'A girl has no name.'
   Brienne   |  Brienne Tarth  |    Knight   | 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'
 Melissandre |   Melissandre   | Necromancer | 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'
-
 - Se o parâmetro não estiver na tabela, a função retorna undefined.
 - Se o parâmetro estiver, a função retorna um objeto no formato abaixo:
-
 {
   name: 'Nome do Personagem',
   class: 'Classe do Personagem' ,
   phrases: ['frase1', 'frase2']
 }
-
 - OBS.: O parâmetro não é CASE SENSITIVE, portanto Arya, ArYa e ARYA tem o mesmo resultado.
-
 Elabore testes para verificar se a função está funcionando de acordo com o proposto.
-
 Parâmetros:
   - Uma string.
-
 Comportamento: 
   - getCharacter('Arya');
-
 Retorno:
 {
   name: 'Arya Stark',
   class: 'Rogue',
   phrases: [ 'Not today', 'A girl has no name.' ]
 }
-
 OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+const characters = {
+  Arya: {
+    name: 'Arya Stark',
+    class: 'Rogue',
+    phrases: ['Not today', 'A girl has no name.'],
+  },
+  Brienne: {
+    name: 'Brienne Tarth',
+    class: 'Knight',
+    phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+  },
+  Melissandre: {
+    name: 'Melissandre',
+    class: 'Necromancer',
+    phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+  },
+};
+
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {    
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
+
     // ESCREVA SEUS TESTES ABAIXO:
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(getCharacter(), undefined);
+    assert.strictEqual(getCharacter(), undefined, `this function has been given undefined as it has not received any input.`);
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    assert.deepStrictEqual(getCharacter('Arya'), characters['Arya'], `getCharacters should return the correct object for the "Arya" parameter`);
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    assert.deepStrictEqual(getCharacter('Brienne'), characters['Brienne'], `getCharacters should return the correct object for the "Brienne" parameter`);
+
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    assert.deepStrictEqual(getCharacter('Melissandre'), characters['Melissandre'], `getCharacters should return the correct object for the "Melissandre" parameter
+    `);
+
     // Teste se a função se os parâmetros não são Case Sensitive.
+    assert.deepStrictEqual(getCharacter('meLissAndrE'), characters['Melissandre'], `getCharacters should return the correct object even if the parameter has a different case`);
+
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    assert.strictEqual(getCharacter('Juka'), undefined, `getCharacters should return undefined when name is not in table`);
   });
 });
