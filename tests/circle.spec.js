0c6d0310fa8a29677f74bd9fe4d2ba6b07485636
Victor hugo Souza <victoraster2014@gmail.com>
2021-08-03 12:09:51 -0300

    commit final dos casos de teste de circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,13 +27,17 @@ describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.deepStrictEqual(circle() ,circle());
+    assert.deepStrictEqual(typeof circle(1) ,'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(circle(5).keys , 3);
+    assert.strictEqual(Object.keys(circle(1)).length , 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.deepStrictEqual(circle(''), undefined);
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    //  2 * Pi * 2 12.56
+    assert.deepStrictEqual(circle(2).circumference,  12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(circle(3).area, 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {area: 3.14 * 3 * 3, circumference: 18.84, radius: 3});
   });
 });
