c572160d5e99a31bfd2fbca4e79ddfa4d4e53828
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-08-16 07:15:33 -0300

    Requisito 6 sucinto

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,5 +18,5 @@
 const createStudent = (name) => ({
   name,
   feedback: () => 'Eita pessoa boa!' });
-
+  
 module.exports = createStudent;
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const product = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(Array.isArray(product), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(product).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const isObject = (array) => (typeof array) === 'object';
+    assert.strictEqual(product.every(isObject), true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(product[0] !== product[1], true);
     // Teste que os dois productIds terminam com 123.
+    const end = (array) => array.details.productId.endsWith('123');
+    assert.strictEqual(product.every(end), true);
   });
 });
